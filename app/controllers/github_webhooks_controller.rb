6265f6cf6a21e92d0d2562061959376d826b5a4c
Jaka <js1929@student.uni-lj.si>
2017-05-01 16:25:23 +0200

    Fix em-proxy spawn.

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -18,15 +18,20 @@ class GithubWebhooksController < ActionController::Base
   private
 
   def start_proxy
-    pid = Process.spawn("ruby #{Rails.root.join('em_proxy.rb')}")
     REDIS.with do |conn|
-      conn.set("proxy_PID", pid)
+      if conn.get("proxy_PID") == "0"
+        pid = Process.spawn("ruby #{Rails.root.join('em_proxy.rb')}")
+        conn.set("proxy_PID", pid)
+      else
+        puts "Already running"  
+      end  
     end
   end
 
   def end_proxy
     REDIS.with do |conn|
-      Process.kill("TERM", conn.get("proxy_PID"))
+      Process.kill("TERM", conn.get("proxy_PID").to_i)
+      conn.set("proxy_PID", "0")
     end
   end    
 end
