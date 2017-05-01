511022970bf99ac6327eade4cce3535434f13452
Jaka <js1929@student.uni-lj.si>
2017-05-01 15:01:28 +0200

    Fix PR label hook.

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -2,12 +2,12 @@ class GithubWebhooksController < ActionController::Base
   include GithubWebhook::Processor
 
   def github_pull_request(payload)
-    if payload['label']['name'] == 'duplicate'
-        puts "Starting proxy"
-    else
-        puts "Stopping proxy"
+    if payload['action'] == 'labeled' && payload['label']['name'] == 'duplicate'
+      puts "Starting proxy"
+    elsif payload['action'] == 'unlabeled' && payload['label']['name'] == 'duplicate'
+      puts "Stopping proxy"
     end    
-  end    
+  end
 
   def webhook_secret(payload)
     #ENV['GITHUB_WEBHOOK_SECRET']
