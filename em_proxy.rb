6a9669d4ed46436f5ef51c4487bd1539c6ddc2b4
jaka <uncoverd@gmail.com>
2017-06-26 12:58:29 +0200

    Change request ID handling.

--- em_proxy.rb
+++ em_proxy.rb
@@ -1,20 +1,17 @@
 require 'em-proxy'
 require 'redis'
-require 'uuid'
 redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 0)
-uuid = UUID.new
-id = 0
 Proxy.start(:host => "0.0.0.0", :port => 8000, :debug => false) do |conn|
   @start = Time.now
   @data = Hash.new("")
-
+  @request_id = nil
   conn.server :production, :host => ENV['MASTER_SHADOW'], :port => 3000    # production, will render resposne
   conn.server :shadow, :host => ENV['SLAVE_SHADOW'], :port => 3000    # testing, internal only
 
   conn.on_data do |data|
-    id = "request-" + uuid.generate.to_s 
-    redis.hset(id, :request, data)
-    redis.hset(id, :time, Time.now.ctime)
+    @request_id = data.scan(/X-Request-Id: .*$/).first
+    redis.hset(@request_id, :request, data)
+    redis.hset(@request_id, :time, Time.now.ctime)
     data
   end
 
@@ -25,8 +22,8 @@ Proxy.start(:host => "0.0.0.0", :port => 8000, :debug => false) do |conn|
 
   conn.on_finish do |name|
     p [:on_finish, name, Time.now - @start]
-    redis.hset(id, :production, @data[:production])
-    redis.hset(id, :shadow, @data[:shadow])
+    redis.hset(@request_id, :production, @data[:production])
+    redis.hset(@request_id, :shadow, @data[:shadow])
     :close if name == :production
   end
 end
