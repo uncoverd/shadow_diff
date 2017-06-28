ede79248a51ee00f9ce0fd16a346f4f683a741fd
jaka <uncoverd@gmail.com>
2017-06-28 19:41:23 +0200

    Remove old comment.

--- app/workers/sync_requests_worker.rb
+++ app/workers/sync_requests_worker.rb
@@ -9,7 +9,6 @@ class SyncRequestsWorker
     redis_responses = RedisResponse.new
     new_responses = redis_responses.all
     new_responses.each do |response|
-        :id, :production_response, :shadow_response, :time, :url
         Response.create(request_id: response.id,, production: response.production_response,
                         shadow: response.shadow_response, url: response.url,
                         time: response.time, Commit.find_or_create_by(hash: response.commit_hash))
