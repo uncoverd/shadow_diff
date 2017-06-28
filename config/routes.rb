e4670ff65c06475b85e8b977e7a5b81fbba1987d
jaka <uncoverd@gmail.com>
2017-06-28 22:12:47 +0200

    Move response and commit actions to a separate controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'shadow#index'
+  root 'commits#index'
   resource :github_webhooks, only: :create, defaults: { formats: :json }
   get '/shadow' => 'shadow#index'
   get '/shadow/:id' => 'shadow#show', as: 'request'
+
+  resources :commits
+  resources :responses
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
