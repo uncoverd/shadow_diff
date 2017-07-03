038605f95a1cb51a008fdd7230e0fa787143f07c
jaka <uncoverd@gmail.com>
2017-07-03 11:59:36 +0200

    Refacture comparator and serializer.

--- lib/raw_response_serializer.rb
+++ lib/raw_response_serializer.rb
@@ -4,15 +4,15 @@ class RawResponseSerializer
     end
 
     def find (key)
-        response = RawResponse.new(key)
-        response.production_response = @connection.hget(key, "production")
-        response.shadow_response = @connection.hget(key, "shadow")
-        response.request = @connection.hget(key, "request")
-        response.time = @connection.hget(key, "time")
-        response.commit_hash = @connection.hget(key, "commit_hash")
-        response.url = @connection.hget(key,"url")
-        response.verb = @connection.hget(key,"verb")
-        response 
+        raw_response = RawResponse.new(key).tap do |obj|
+            response.production_response = @connection.hget(key, "production")
+            response.shadow_response = @connection.hget(key, "shadow")
+            response.request = @connection.hget(key, "request")
+            response.time = @connection.hget(key, "time")
+            response.commit_hash = @connection.hget(key, "commit_hash")
+            response.url = @connection.hget(key,"url")
+            response.verb = @connection.hget(key,"verb")
+        end
     end    
 
 end
--- lib/response_comparator.rb
+++ lib/response_comparator.rb
@@ -9,22 +9,20 @@ class ResponseComparator
     end
 
     def calculate_score
-        indexes = diff_indexes
-        puts diff_indexes
+        lines = changed_lines
         ComparisonResult.where(response: @response).destroy_all
-        indexes.each do |index, line|
+        lines.each do |line|
             missing_rule = true
             @rules.each do |rule|
-                line_score = rule.evaluate(@diff[index])
+                line_score = rule.evaluate(line)
                 @score += line_score
                 if line_score >= 0
-                    ComparisonResult.create(response: @response, rule: rule,
-                                        line_score: line_score, line: line)
+                    ComparisonResult.create(response: @response, rule: rule, line_score: line_score, line: line)
                     missing_rule = false
                 end    
             end
             if missing_rule
-                create_missing_result(line)
+                create_comparison_result(line)
             end    
         end
         score
@@ -32,7 +30,7 @@ class ResponseComparator
 
     private
 
-    def create_missing_result(line)
+    def create_comparison_result(line)
         name = ''
         if line[0] == '+'
             name = 'Added'
@@ -40,18 +38,17 @@ class ResponseComparator
             name = 'Removed'
         end        
         rule = Rule.create(modifier: -1, name: name, regex_string: line, url: @response.url, commit: @response.commit)
-        ComparisonResult.create(response: @response, rule: rule,
-                                        line_score: -1, line: line)
+        ComparisonResult.create(response: @response, rule: rule, line_score: -1, line: line)
     end    
 
-    def diff_indexes
-        indexes = []
-        @diff = Diffy::Diff.new(@response.production, @response.shadow).to_a#.each_chunk.to_a
-        @diff.each_with_index do |line, index|
+    def changed_lines
+        lines = []
+        diff = Diffy::Diff.new(@response.production, @response.shadow).to_a
+        diff.each do |line|
             if line[/^\+/] or line[/^-/]
-                indexes << [index,line]
+               lines << line
             end
         end
-        indexes  
+        lines  
     end
 end    
