d930e0a392da5e4f5ebef31821982b5c24836e90
jaka <uncoverd@gmail.com>
2017-07-04 13:20:50 +0200

    Add response color on commit show view.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,8 @@
 class Response < ApplicationRecord
     belongs_to :commit
     belongs_to :url
+
+    def color
+        score >= 0 ? "success" : "danger"
+    end
 end
--- app/models/rule.rb
+++ app/models/rule.rb
@@ -5,34 +5,6 @@ class Rule < ApplicationRecord
   enum status: [:active, :missing, :default]
   enum action: [:change, :add, :remove]
 
-  PREFIXES = {  'add' => ['+'],
-                'remove' => ['-'],
-                'change' => ['+','-']}
-
-  LINE_CHANGES = {  'add' => 1,
-                    'remove' => 1,
-                    'change' => 2}
-
-  def evaluate(lines)
-    detected_prefixes = []
-    detected_lines = []
-  
-    lines.each do |line|
-      PREFIXES[action].each_with_index do |prefix, index|
-        if line[regex(prefix)]
-          detected_lines << line
-          detected_prefixes[index] = true
-        end  
-      end 
-    end
-
-    if detected_prefixes.count(true) == LINE_CHANGES[action]
-      [modifier, detected_lines]
-    else
-      [-1, []]
-    end  
-  end
-
   def self.default_regexes
     [['ETag', 'ETag'], ['Set-Cookie', 'SetCookie'], ['X-Runtime', 'X-Runtime'], ['<meta content=(.)* name="csrf-token" />', "CSRF token"]]
   end
@@ -41,10 +13,5 @@ class Rule < ApplicationRecord
     -1
   end     
 
-  private
   
-  def regex(prefix)
-    Regexp.new(Regexp.escape(prefix + regex_string))
-  end 
-
 end
