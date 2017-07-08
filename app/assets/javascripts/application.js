9994fa83726b1aa2330d91eff99106bf1b222bda
jaka <uncoverd@gmail.com>
2017-07-08 11:09:41 +0200

    Fix AdminLTE height when using Turbolinks.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,13 @@
 //= require app.js
 //= require_tree .
 
+var ready = function () {
+  var o;
+  o = $.AdminLTE.options;
+  if (o.sidebarPushMenu) {
+    $.AdminLTE.pushMenu.activate(o.sidebarToggleSelector);
+  }
+  return $.AdminLTE.layout.activate();
+};
+document.addEventListener('turbolinks:load', ready);
+
