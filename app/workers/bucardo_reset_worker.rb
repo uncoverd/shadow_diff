6b5bd334f6b3f08d760866eae5c9add60a98d381
Jaka <js1929@student.uni-lj.si>
2017-07-10 14:04:28 +0200

    Fix line ordering.

--- app/workers/bucardo_reset_worker.rb
+++ app/workers/bucardo_reset_worker.rb
@@ -6,8 +6,9 @@ class BucardoResetWorker
   end
 
   def perform
-    redis.set("bucardo_working", "true")
     redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 0)
+    redis.set("bucardo_working", "true")
+
     #ENV['MASTER_DB'] ENV['SLAVE_DB']
     controller = BucardoController.new('128.199.59.160','188.166.15.157')
     controller.reset_slave_db
