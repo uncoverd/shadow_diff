27227a6336691067eee981e873c7566c2599046c
Jaka <js1929@student.uni-lj.si>
2017-07-17 13:19:52 +0200

    Add pull request url link.

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -7,7 +7,7 @@ class GithubWebhooksController < ActionController::Base
     commit_hash = payload['pull_request']['head']['sha']
     author = payload['pull_request']['user']['login']
     title = payload['pull_request']['title']
-    url = payload['pull_request']['url']
+    url = payload['pull_request']['html_url']
     puts repo_name
     puts commit_hash
     
