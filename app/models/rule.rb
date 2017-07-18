2627e997aaebfb5b78c961d4db525a8ab4829816
Jaka <js1929@student.uni-lj.si>
2017-07-18 19:23:32 +0200

    Add default metric ignore rules.

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -14,7 +14,9 @@ class Rule < ApplicationRecord
 
   def self.default_regexes
     [['ETag', 'ETag'], ['Set-Cookie', 'SetCookie'], ['X-Runtime', 'X-Runtime'], ['<meta content=(.)* name="csrf-token" />', "CSRF token"],
-    ['CSRF form token','    <form accept-charset=\"UTF-8\" action=\"/sessions\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /><input name=\"authenticity_token\" type=\"hidden\" value=\"(.*?)\" /></div>']]
+    ['CSRF form token',
+    '    <form accept-charset=\"UTF-8\" action=\"/sessions\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /><input name=\"authenticity_token\" type=\"hidden\" value=\"(.*?)\" /></div>'],
+    ['X-View-Runtime', 'X-View-Runtime'],['X-Db-Runtime', 'X-Db-Runtime'],['X-Sql-Queries', 'X-Sql-Queries']]
   end
 
   def self.missing_modifier
