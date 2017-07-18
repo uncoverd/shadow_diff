c409121c6ac09fb2de8500600a972ba74d40b332
Jaka <js1929@student.uni-lj.si>
2017-07-18 19:35:57 +0200

    Fix handling of nil metric values.

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -2,28 +2,29 @@ module ResponsesHelper
 
     EPSILONS = {:sql => 1, :db_runtime => 70, :view_runtime => 50}
 
+    #use to_f method to cast nil to 0
     def sql_requests_diff
-        (production_sql_requests - shadow_sql_requests).round(0)
+        (production_sql_requests.to_f - shadow_sql_requests.to_f).round(0)
     end
     
     def view_runtime_diff
-        production_view_runtime - shadow_view_runtime
+        production_view_runtime.to_f - shadow_view_runtime.to_f
     end
 
     def db_runtime_diff
-        production_db_runtime - shadow_db_runtime
+        production_db_runtime.to_f - shadow_db_runtime.to_f
     end
 
     def view_runtime_diff_output
-        view_runtime_diff >= 0 ? "+ #{view_runtime_diff}" : "-#{view_runtime_diff}"
+        view_runtime_diff >= 0 ? "+ #{view_runtime_diff}" : view_runtime_diff
     end
     
     def db_runtime_diff_output
-        db_runtime_diff >= 0 ? "+ #{db_runtime_diff}" : "-#{db_runtime_diff}"
+        db_runtime_diff >= 0 ? "+ #{db_runtime_diff}" : db_runtime_diff
     end
 
     def sql_requests_diff_output
-        sql_requests_diff >= 0 ? "+ #{sql_requests_diff}" : "-#{sql_requests_diff}"
+        sql_requests_diff >= 0 ? "+ #{sql_requests_diff}" : sql_requests_diff
     end
     
 end
